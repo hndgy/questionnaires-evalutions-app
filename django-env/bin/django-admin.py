e9009606b2343f5edcff5e0539e65fd1ff273126
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2020-04-24 14:45:33 +0200

    Initial commit

---
+++ django-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hndgy/Desktop/cours/L3 MIAGE/projet_stage/questionnaireevaluation/django-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
